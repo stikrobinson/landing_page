6f214dc14bdae947106f2294b032b49d3fff0091
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-06-11 15:38:35 +0000

    arreglos en contacto

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+"use strict";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const asignarEnBotones = () => {
+  const reservarClase = document.querySelector("#reservar-clase");
+  const reservarClases = document.querySelector("#reservar-clases");
+  const reservarPresencial = document.querySelector("#reservar-presencial");
+  const select = document.querySelector("select[name='subject']");
 
-setupCounter(document.querySelector('#counter'))
+
+  reservarClase.addEventListener("click", () => {
+    select.selectedIndex = 2;
+  });
+
+  reservarClases.addEventListener("click", () => {
+    select.selectedIndex = 3;
+  });
+
+  reservarPresencial.addEventListener("click", () => {
+    select.selectedIndex = 4;
+  });
+
+};
+
+(function() {
+  asignarEnBotones();
+})();
